b2b659677cc64fda37366c83f80aeb37d3dbe594
fangchaolai <fangchaolai>
2017-06-17 15:31:57 +0800

    实现viewpager一屏显示多页面方案

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # InterestingViewPager
 多彩的viewpager
+## viewPager一屏显示多个页面
--- app/src/main/java/com/viewpager/ranger/MainActivity.java
+++ app/src/main/java/com/viewpager/ranger/MainActivity.java
@@ -1,13 +1,61 @@
 package com.viewpager.ranger;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
+/**
+ * viewpager一屏显示多个页面
+ * Android:clipChildren="false"--子view在绘制时不要裁切它的显示范围
+ */
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager vp;
+
+    private int[] imgResources = {R.mipmap.a1, R.mipmap.a2, R.mipmap.a3, R.mipmap.a4, R.mipmap.a5};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        vp = (ViewPager) findViewById(R.id.vp);
+
+        //设置page间距
+        vp.setPageMargin(20);
+        vp.setOffscreenPageLimit(3);
+
+        vp.setAdapter(new MyAdapter());
+    }
+
+
+    class MyAdapter extends PagerAdapter {
+
+        @Override
+        public int getCount() {
+            return imgResources.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            ImageView view = new ImageView(MainActivity.this);
+            view.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            view.setImageResource(imgResources[position]);
+            container.addView(view);
+            return view;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
     }
 }
